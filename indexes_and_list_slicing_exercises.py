48ee45a5a6cc11f004cb12e8867d199574e7283a
darknight9797 <81421227+darknight9797@users.noreply.github.com>
2026-01-07 17:30:28 +0530

    Add files via upload

---
+++ indexes_and_list_slicing_exercises.py
@@ -0,0 +1,19 @@
+list = [[0,2],[4,6],[8,10],[12,14]]
+
+print(list[0])
+
+print(list[3][1])
+
+list2 = ["chair", "table", "desk", "lamp", "bed"]
+
+print(list2[-5])
+
+print("Most people own at least " + str(list[0][1]) + " " + list2[0] + "s.")
+
+list3 = [0.98,8.76,6.54,4.32]
+
+print(list3[1:4])
+
+print(list3[1:3])
+
+print(list3[:2])

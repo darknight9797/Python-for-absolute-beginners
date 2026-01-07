d216c15f9882ce77f9c722c37068814c437265c2
darknight9797 <81421227+darknight9797@users.noreply.github.com>
2026-01-07 17:33:07 +0530

    Update 20) list_exercises.py

--- 20) list_exercises.py
+++ 20) list_exercises.py
@@ -1,7 +1,16 @@
-mixed = [10, 4.97, True, "mountain", [9, 8, 7]]
-li_str = list("cheese")
+#1. Create a variable and assign it a list that contains an integer, a float, a Boolean value, a string, and a list of 3 integers.
 
-print("e" in li_str)
+#2.Create another variable and assign it a call of the list() function with a string as its argument.
+
+#3. Use the keyword "in" to check if the letter "e" is in the list assigned to the variable from step 2 and print the result.
+
+#4. Use the keyword "not in" to check if the letter "a" is not in the list assigned to the variable from step 2 and print the result.
+
+mixed = [10, 4.97, True, "mountain", [9, 8, 7]] #1
+li_str = list("cheese") #2
+
+print("e" in li_str) #3
 
 
-print("a" not in li_str)
+print("a" not in li_str) #4
+

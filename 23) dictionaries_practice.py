3ba8930e6beed597f403abb1c4e8f961affa22bd
darknight9797 <81421227+darknight9797@users.noreply.github.com>
2026-01-23 16:20:20 +0530

    Implement dictionary operations in dictionaries_practice.py
    Added dictionary operations including key access and membership checks.

---
+++ 23) dictionaries_practice.py
@@ -0,0 +1,13 @@
+#1) create a variable and assign it a dictionary that has 5 key value pairs
+
+#2) print and access the value held at the third key in the dictionary
+
+#3) use the in keyword to check if a key appears in the dictionary and print the result
+
+#4) use not in to check if a key does not appear in the dictionary and print the result
+
+dict = {"Jon": 25, "Arya": 18, "Sansa": 22, "Bran": 20, "Robb": 24}
+print(dict["Sansa"])
+print("Jon" in dict)
+
+print("Cersei" not in dict)
